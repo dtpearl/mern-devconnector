5acc92771de4f89b995ad50e9438b1758f86e149
David Pearl <davidtpearl@gmail.com>
2019-11-18 22:15:21 +1100

    Connect MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (reqest, response) => response.send('API Running'));
+// Connect Database
+connectDB();
+
+app.get('/', (reqest, response) =>
+  response.send('API Running, you better catch it!')
+);
 
 const PORT = process.env.PORT || 5000;
 
