da03872c9420c28dd3fa1cd705944b8f1d1280a9
David Pearl <davidtpearl@gmail.com>
2019-11-19 23:15:03 +1100

    POST route - Register a user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (request, response) => {
-  console.log(request.body);
-  response.send('User route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({
+        errors: errors.array()
+      });
+    }
+    response.send('User route');
+  }
+);
 
 module.exports = router;
