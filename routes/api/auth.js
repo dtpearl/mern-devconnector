16736435a2563b21f0d3ddfabb84e2e746bc2211
David Pearl <davidtpearl@gmail.com>
2020-04-05 17:12:15 +1000

    Create, update and delete profile routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -71,7 +71,7 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 3600000 },
+        { expiresIn: 3600000 }, // TODO Set this to expire
         (err, token) => {
           if (err) throw err;
           response.json({ token });
